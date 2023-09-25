33f798e35e4735987bad571d5fc6a7815d5ec660
yassine lamouadden <mouden529@gmail.com>
2023-09-25 17:37:38 +0100

    search string naive implementation + intro KMP algo

--- Searshing algorithms/search.js
+++ Searshing algorithms/search.js
@@ -45,4 +45,42 @@ const binarySearchRecur = (arr, target) => {
 };
 
 // remember the time complexity is O(log(n)) example if we have 32 elements log(32)=5 because pow(2,5) is 32
-c(binarySearchRecur([1, 2, 3, 4, 5], 3));
+
+// search string naive implementation
+const searchString = (longerString, targetString) => {
+  let counter = 0;
+  for (let i = 0; i < longerString.length; i++) {
+    counter = 0;
+
+    if (longerString[i] === targetString[0]) {
+      for (let j = 0; j < targetString.length; j++) {
+        if (longerString[i + j] !== targetString[j]) break;
+        counter++;
+      }
+      if (counter === targetString.length) break;
+    }
+  }
+
+  if (counter === targetString.length) return true;
+  else return false;
+};
+
+//KMP substring search  : time complexity O(m+n) with m length of longString and n length of targetString
+
+const buildPrefixTable = () => {};
+
+c(searchString("yassine lamouadden", "yassine lamouadden"));
+
+/* As a web developer, you may consider using the Knuth-Morris-Pratt (KMP) algorithm for substring searching in scenarios where you need to perform efficient and fast substring search operations on strings. Here are some situations where using the KMP algorithm or other efficient substring search algorithms might be beneficial:
+
+Large Texts: If you're dealing with large text data, such as searching for keywords or phrases in lengthy articles or documents, using a more efficient algorithm like KMP can significantly improve search performance compared to simple linear search methods.
+
+Search Engines: If you're building a search engine or search functionality for a website, efficient substring search algorithms like KMP can help improve the speed of search queries, especially when dealing with a large corpus of documents.
+
+Auto-Completion: Implementing auto-completion or suggestions in search bars or input fields can benefit from faster substring searching. KMP can help provide real-time suggestions as users type.
+
+Text Editors: If you're developing a web-based text editor or code editor, efficient substring search algorithms can be essential for features like "Find" or "Find and Replace."
+
+Data Processing: In web applications that involve text processing or data mining, substring searching can be a common operation. Using an efficient algorithm like KMP can speed up data processing tasks.
+
+Pattern Matching: When dealing with pattern matching in strings, such as detecting specific patterns or regular expressions, the KMP algorithm can be useful in reducing the time complexity of the matching process. */
